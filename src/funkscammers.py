34a8caee99489d19a850b057d221598b4c0c8dd1
hor00s <hor00s199@gmail.com>
2023-06-08 16:44:16 +0300

    Minor change

--- src/funkscammers.py
+++ src/funkscammers.py
@@ -1,7 +1,9 @@
 #!/bin/env python3
 import sys
 from program.program import main  # type: ignore
-
+from bot.bot import Bot
+from generals.constants import SCAM_SAMPLES
+from generals.actions import load_samples
 
 """
 This program is made purely for fun in an attempt
@@ -13,6 +15,17 @@ profitable in any way and is completly open-source (as you see)!
 
 
 if __name__ == '__main__':
+    if sys.argv[1] == 'test':
+        # python3 funkscammers.py test "A random sentence"
+        samples = load_samples(SCAM_SAMPLES)
+
+        bot = Bot('username', 'password')
+        for sample in samples:
+            sentence = sys.argv[2]
+            rate = bot.get_rate(sample, sentence)
+            print(f'{sentence = }\n{sample = }\n{rate = }\n')
+        sys.exit(0)
+
     sys.exit(
         main(*sys.argv)
     )
